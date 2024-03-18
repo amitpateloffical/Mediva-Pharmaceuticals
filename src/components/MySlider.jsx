861f308a771f84580de46bcfaa4de6cd72dff512
pankaj9332 <pankaj.jat@vidyagxp.com>
2024-03-18 16:32:52 +0530

    changes in slider

--- src/components/MySlider.jsx
+++ src/components/MySlider.jsx
@@ -43,10 +43,10 @@ const MySlider = () => {
     <Slider {...settings} >
       {/* First Slide */}
 
-      <img height={"500px"} src="https://www.moralpharma.com/data1/images/Pharmaslider1.png" />
+      <img height={"500px"} src="https://www.aranca.com/assets/uploads/blogs/pharmacomban.jpg" />
       <img  height={"500px"} src="https://img.freepik.com/free-vector/pharmaceutical-medicine-healthcare-template-vector-social-media-post_53876-117769.jpg?size=626&ext=jpg&ga=GA1.1.1884802103.1674757283&semt=sph"/>
-      <img height={"500px"} src="https://img.freepik.com/free-photo/various-pills-hand_23-2147983107.jpg?size=626&ext=jpg&ga=GA1.1.1884802103.1674757283&semt=sph" />
-     <img height={"500px"} src="https://img.freepik.com/free-vector/healthcare-background-with-medical-symbols-hexagonal-frame_1017-26363.jpg?size=626&ext=jpg&ga=GA1.2.1884802103.1674757283&semt=sph" />
+      <img height={"500px"} src="https://media.licdn.com/dms/image/C4E12AQFgbfctoLOcRQ/article-cover_image-shrink_720_1280/0/1613483589558?e=2147483647&v=beta&t=Tjy3ZAvAHDKvlNL_9fMGcRq42Ocu4546yIJH9_mOjU8" />
+     <img height={"500px"} src="https://www.pharmaceutical-technology.com/wp-content/uploads/sites/24/2021/06/Important-priorities-for-pharma-companies.jpg" />
       {/* <div className="container-fluid pt-5  hero-header mb-5">
         <div className="container pt-5">
           <div className="row g-5 pt-5">
